5313d73533507d822845a2740f14cdf0d4b01e1f
Gergely Korcsak <razzero29@gmail.com>
2023-05-27 13:46:35 +0200

    model cration and files moved

---
+++ model.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+
+TEST_PATH = 'Synthetic_MICCAI2020_dataset/Video_01/images'
+TRAIN_PATH ='Synthetic_MICCAI2020_dataset/Video_01/green_screen'
+MASK_PATH  ='Synthetic_MICCAI2020_dataset/Video_01/ground_truth'
+
+IMG_WIDTH =  512 #701
+IMG_HEIGHT = 512 #538
+IMG_CHANEL = 3
+
+MODEL_PATH   = 'models/model_for_hus.h5'
+WEIGHTS_PATH = 'models/model_for_hus_weights.h5'
+
+def create_model():
+    inputs = tf.keras.layers.Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANEL))
+    s= tf.keras.layers.Lambda(lambda x: x/255)(inputs)
+
+    #Contraction path
+    c1 =  tf.keras.layers.Conv2D(filters=16, kernel_size=3,activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = tf.keras.layers.Dropout(0.1)(c1)
+    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = tf.keras.layers.MaxPooling2D((2,2))(c1)
+
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = tf.keras.layers.Dropout(0.1)(c2)
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+    
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = tf.keras.layers.Dropout(0.2)(c3)
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+    
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = tf.keras.layers.Dropout(0.2)(c4)
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+    
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = tf.keras.layers.Dropout(0.3)(c5)
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+    #Expansive path 
+    u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = tf.keras.layers.concatenate([u6, c4])
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = tf.keras.layers.Dropout(0.2)(c6)
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    
+    u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = tf.keras.layers.Dropout(0.2)(c7)
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    
+    u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = tf.keras.layers.Dropout(0.1)(c8)
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    
+    u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = tf.keras.layers.Dropout(0.1)(c9)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+    
+    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
--- tanitas.py
+++ tanitas.py
@@ -1,3 +1,4 @@
+import model as md
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as  plt
@@ -10,40 +11,35 @@ import random
 from PIL import Image
 from skimage.io import imread, imshow
 from skimage.transform import resize
-TEST_PATH = 'Synthetic_MICCAI2020_dataset/Video_01/images'
-TRAIN_PATH ='Synthetic_MICCAI2020_dataset/Video_01/green_screen'
-MASK_PATH  ='Synthetic_MICCAI2020_dataset/Video_01/ground_truth'
-IMG_WIDTH =  512 #701
-IMG_HEIGHT = 512 #538
-IMG_CHANEL = 3
-train_ids = os.listdir(MASK_PATH)
-test_ids = os.listdir(MASK_PATH)
-X_train = np.zeros((len(train_ids),IMG_HEIGHT,IMG_WIDTH,IMG_CHANEL), dtype=np.uint8) #feltölti 0-kal
-Y_train = np.zeros((len(train_ids),IMG_HEIGHT, IMG_WIDTH, 1), dtype = np.bool)
+
+train_ids = os.listdir(md.MASK_PATH)
+test_ids = os.listdir(md.MASK_PATH)
+X_train = np.zeros((len(train_ids), md.IMG_HEIGHT, md.IMG_WIDTH, md.IMG_CHANEL), dtype=np.uint8) #feltölti 0-kal
+Y_train = np.zeros((len(train_ids), md.IMG_HEIGHT, md.IMG_WIDTH, 1), dtype = np.bool)
 
 for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):   
-    path = TRAIN_PATH +'/' + id_
-    img = imread(path)[:,:,:IMG_CHANEL]  
-    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    path = md.TRAIN_PATH +'/' + id_
+    img = imread(path)[:,:,:md.IMG_CHANEL]  
+    img = resize(img, (md.IMG_HEIGHT, md.IMG_WIDTH), mode='constant', preserve_range=True)
     X_train[n] = img  #Fill empty X_train with values from img
     
-    mask= np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)    
-    t_path=MASK_PATH +'/' + id_    
+    mask= np.zeros((md.IMG_HEIGHT, md.IMG_WIDTH, 1), dtype=np.bool)    
+    t_path=md.MASK_PATH +'/' + id_    
     # Replace with the desired threshold value
     #binary_image = cv2.cvtColor(imread(t_path), cv2.COLOR_RGB2GRAY)    
     bi = imread(t_path)[:,:,:1]
-    bi = resize(bi,(IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
-    mask = np.reshape(bi,(IMG_HEIGHT, IMG_WIDTH, 1))
+    bi = resize(bi,(md.IMG_HEIGHT, md.IMG_WIDTH), mode='constant', preserve_range=True)
+    mask = np.reshape(bi,(md.IMG_HEIGHT, md.IMG_WIDTH, 1))
     
-    Y_train[n] = mask  
+    Y_train[n] = mask
 
-X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANEL), dtype=np.uint8)
+X_test = np.zeros((len(test_ids), md.IMG_HEIGHT, md.IMG_WIDTH, md.IMG_CHANEL), dtype=np.uint8)
 sizes_test = []
 print('load test images')
 for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
-    path_husos = TEST_PATH +'/' + id_
+    path_husos = md.TEST_PATH +'/' + id_
     img = imread(path_husos)
-    img =resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    img =resize(img, (md.IMG_HEIGHT, md.IMG_WIDTH), mode='constant', preserve_range=True)
     X_test[n] = img
 print('Done!')
     
@@ -57,62 +53,9 @@ if os.path.exists("model_for_hus.h5"):
 else:
     #Build the model
     start = time.time()
-    inputs = tf.keras.layers.Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANEL))
-    s= tf.keras.layers.Lambda(lambda x: x/255)(inputs)
+    
+    model = md.create_model()
 
-    #Contraction path
-    c1 =  tf.keras.layers.Conv2D(filters=16, kernel_size=3,activation='relu', kernel_initializer='he_normal', padding='same')(s)
-    c1 = tf.keras.layers.Dropout(0.1)(c1)
-    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
-    p1 = tf.keras.layers.MaxPooling2D((2,2))(c1)
-
-    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
-    c2 = tf.keras.layers.Dropout(0.1)(c2)
-    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
-    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
-    
-    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
-    c3 = tf.keras.layers.Dropout(0.2)(c3)
-    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
-    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
-    
-    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
-    c4 = tf.keras.layers.Dropout(0.2)(c4)
-    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
-    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
-    
-    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
-    c5 = tf.keras.layers.Dropout(0.3)(c5)
-    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
-
-    #Expansive path 
-    u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
-    u6 = tf.keras.layers.concatenate([u6, c4])
-    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
-    c6 = tf.keras.layers.Dropout(0.2)(c6)
-    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
-    
-    u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
-    u7 = tf.keras.layers.concatenate([u7, c3])
-    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
-    c7 = tf.keras.layers.Dropout(0.2)(c7)
-    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
-    
-    u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
-    u8 = tf.keras.layers.concatenate([u8, c2])
-    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
-    c8 = tf.keras.layers.Dropout(0.1)(c8)
-    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
-    
-    u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
-    u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
-    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
-    c9 = tf.keras.layers.Dropout(0.1)(c9)
-    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
-    
-    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
-    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     model.summary()
     #model.save('/model_for_hus.h5')
     checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_hus.h5', verbose = 1, save_best_only=True)
