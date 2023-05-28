f4f227dde7b45177e4d0b3f55365176e53c7bffd
DaniKere <133917357+DaniKere@users.noreply.github.com>
2023-05-28 22:01:29 +0200

    stream

---
+++ model_camera.py
@@ -0,0 +1,184 @@
+import tensorflow as tf
+from tqdm import tqdm
+import os
+import numpy as np
+from skimage.io import imread
+from skimage.io import imshow
+from skimage.transform import resize
+
+# Image pathsSynthetic_MICCAI2020_dataset\others\Video_17\images
+TEST_PATH =         'Synthetic_MICCAI2020_dataset/others/Video_17/images'
+INDIVIDUAL_IMG_PATH =  'én.PNG'
+#TRAIN_PATH =          #'Synthetic_MICCAI2020_dataset/Video_01/green_screen'
+#MASK_PATH  =          #'Synthetic_MICCAI2020_dataset/Video_01/ground_truth'
+FOLDER_PATH = 'Synthetic_MICCAI2020_dataset'
+IMG_WIDTH =  512 #701
+IMG_HEIGHT = 512 #538
+IMG_CHANEL = 3
+TRAIN_SEP = 100
+
+WEIGHTS_PATH = 'models/model_for_hus_weights.h5'
+
+numeric = 0
+def debug_print():
+    global numeric
+    print(numeric)
+    numeric += 1
+
+def model_exists():
+    return os.path.exists(WEIGHTS_PATH)
+
+def create_model():
+    debug_print()
+    inputs = tf.keras.layers.Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANEL))
+    s= tf.keras.layers.Lambda(lambda x: x/255)(inputs)
+
+    debug_print()
+    #Contraction path
+    c1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3,activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    debug_print()
+    c1 = tf.keras.layers.Dropout(0.1)(c1)
+    debug_print()
+    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    debug_print()
+    p1 = tf.keras.layers.MaxPooling2D((2,2))(c1)
+
+    debug_print()
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = tf.keras.layers.Dropout(0.1)(c2)
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+    
+    debug_print()
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = tf.keras.layers.Dropout(0.2)(c3)
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+    
+    debug_print()
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = tf.keras.layers.Dropout(0.2)(c4)
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+    
+    debug_print()
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = tf.keras.layers.Dropout(0.3)(c5)
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    debug_print()
+    #Expansive path 
+    u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = tf.keras.layers.concatenate([u6, c4])
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = tf.keras.layers.Dropout(0.2)(c6)
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    
+    debug_print()
+    u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = tf.keras.layers.Dropout(0.2)(c7)
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    
+    debug_print()
+    u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = tf.keras.layers.Dropout(0.1)(c8)
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    
+    debug_print()
+    u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = tf.keras.layers.Dropout(0.1)(c9)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+    
+    debug_print()
+    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    debug_print()
+    return model
+
+def train_model(model: tf.keras.Model, validation_split, batch_size, epochs, callbacks):
+    image_folder = [f for f in os.listdir(FOLDER_PATH) if os.path.isdir(os.path.join(FOLDER_PATH, f))]
+    results = []
+    for all_f in image_folder:
+        if "video" in all_f.lower():     
+            TRAIN_PATH =   FOLDER_PATH +"/"+ all_f +"/images"      
+            MASK_PATH  =   FOLDER_PATH +"/"+ all_f +"/ground_truth"               
+            X_train, Y_train = load_traning_images(TRAIN_PATH, MASK_PATH)
+            for x in range(0, len(X_train), TRAIN_SEP):
+                x_tmp = X_train[x:x+TRAIN_SEP]
+                y_tmp = Y_train[x:x+TRAIN_SEP]
+                results.append(model.fit(x_tmp, y_tmp, validation_split=validation_split, batch_size=batch_size, epochs=epochs, callbacks=callbacks))
+    return results
+
+def save_model(model: tf.keras.models.Model):
+    model.save_weights(WEIGHTS_PATH)
+
+def load_traning_images(train_path, mask_path, maxnum = -1):
+    train_ids = os.listdir(mask_path)
+    if maxnum >= 0 and len(train_ids) > maxnum:
+        load_len = maxnum
+    else:
+        load_len = len(train_ids)
+
+    X_train = np.zeros((load_len, IMG_HEIGHT, IMG_WIDTH, IMG_CHANEL), dtype=np.uint8) #feltölti 0-kal
+    Y_train = np.zeros((load_len, IMG_HEIGHT, IMG_WIDTH, 1), dtype=bool)
+
+    for n, id_ in tqdm(enumerate(train_ids), total=load_len):
+        if n >= load_len:
+            break
+        path = train_path + '/' + id_
+        img = imread(path)[:,:,:IMG_CHANEL]
+        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_train[n] = img  #Fill empty X_train with values from img
+        
+        mask   = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=bool)
+        t_path = mask_path + '/' + id_   
+        bi     = imread(t_path)[:,:,:1]
+        bi     = resize(bi, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        mask   = np.reshape(bi, (IMG_HEIGHT, IMG_WIDTH, 1))
+        
+        Y_train[n] = mask
+
+    return X_train, Y_train
+
+def load_images(img_path, ind=-1 , max_img_num = -1):
+    if ind != -1: 
+        X_test = np.zeros((1, IMG_HEIGHT, IMG_WIDTH, IMG_CHANEL), dtype=np.uint8)
+        path_husos =  ind
+        img = imread(path_husos)[:,:,:IMG_CHANEL]
+        img =resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_test[0] = img
+    else:
+        test_ids = os.listdir(img_path)
+        
+        if max_img_num >= 0 and len(test_ids) > max_img_num:
+            load_len = max_img_num
+        else:
+            load_len = len(test_ids)
+
+            X_test = np.zeros((load_len, IMG_HEIGHT, IMG_WIDTH, IMG_CHANEL), dtype=np.uint8)
+            print('load test images')
+            for n, id_ in tqdm(enumerate(test_ids), total=load_len):
+                if n >= load_len:
+                    break
+                path_husos = img_path + '/' + id_
+                img = imread(path_husos)
+                img =resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+                X_test[n] = img
+
+    return X_test
+def load_images_camera(frame):
+    X_test = np.zeros((1, IMG_HEIGHT, IMG_WIDTH, IMG_CHANEL), dtype=np.uint8)    
+    frame = frame[:,:,:IMG_CHANEL]
+    imshow(frame)
+    img =resize(frame, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_test[0] = img
+    return X_test
+    
---
+++ tanitas_camera.py
@@ -0,0 +1,55 @@
+import model as md
+import make_markup_images as mp
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as  plt
+import time
+import random
+import numpy as np
+from skimage.io import imshow
+
+
+#X_train = np.zeros((0, md.IMG_HEIGHT, md.IMG_WIDTH, md.IMG_CHANEL), dtype=np.uint8) #feltölti 0-kal
+#Y_train = np.zeros((0, md.IMG_HEIGHT, md.IMG_WIDTH, 1), dtype=bool)
+
+start = time.time()
+model = md.create_model()
+
+if md.model_exists():
+    print("Betölt")
+    model.load_weights(md.WEIGHTS_PATH)
+else:
+    print("Build")
+
+    #Build the model
+    model.summary()
+    checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_hus.h5', verbose = 1, save_best_only=True)
+    callbacks = [tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'), tf.keras.callbacks.TensorBoard(log_dir = '/')]
+    results = md.train_model(model=model, validation_split=0.0001, batch_size=16, epochs=5, callbacks=callbacks)
+    print('With params: {}'.format(results))
+
+    md.save_model(model)
+    end = time.time()
+    tdiff = end - start
+    print('Finnished building model in {}'.format(tdiff))
+    
+end = time.time()
+tdiff = end - start
+print('Finnished model in {}'.format(tdiff))
+print('')
+
+
+X_test = md.load_images(md.TEST_PATH, 300)
+
+preds_test  = model.predict(X_test)
+
+ 
+
+ix = random.randint(0, len(preds_test) - 1)
+
+for i in range(0, len(preds_test)-1):
+    img         = X_test[i]
+    overlayable = np.squeeze(((preds_test[i] > .5) * 255).astype(np.uint8))
+    imshow(mp.create_overlayed_image(img, overlayable))
+    plt.show()
+
